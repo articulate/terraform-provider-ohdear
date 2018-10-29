25406a562da8f35a5c3594821bb716ef62e265ef
Chaz <chaz@plaidpotion.com>
2018-10-29 11:24:46 -0400

    use ohdear-sdk

--- ohdear/resource_site_test.go
+++ ohdear/resource_site_test.go
@@ -8,6 +8,22 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
+func TestAccOhdearSiteCreate(t *testing.T) {
+	ri := acctest.RandInt()
+	resource.Test(t, resource.TestCase{
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testConfigForOhdearSiteCreate(ri),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fmt.Sprintf("ohdear_site.test-%d", ri), "team_id", "1820"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("ohdear_site.test-%d", ri), "url", fmt.Sprintf("http://www.test-%d.com", ri)),
+				),
+			},
+		},
+	})
+}
+
 func TestAccOhdearSiteLifecycle(t *testing.T) {
 	ri := acctest.RandInt()
 	resource.Test(t, resource.TestCase{
@@ -16,12 +32,14 @@ func TestAccOhdearSiteLifecycle(t *testing.T) {
 			{
 				Config: testConfigForOhdearSiteCreate(ri),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr(fmt.Sprintf("ohdear_site.test-%d", ri), "team_id", "1775"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("ohdear_site.test-%d", ri), "team_id", "1820"),
+					resource.TestCheckResourceAttr(fmt.Sprintf("ohdear_site.test-%d", ri), "url", fmt.Sprintf("http://www.test-%d.com", ri)),
 				),
 			},
 			{
 				Config: testConfigForOhdearSiteUpdate(ri),
 				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fmt.Sprintf("ohdear_site.test-%d", ri), "team_id", "1820"),
 					resource.TestCheckResourceAttr(fmt.Sprintf("ohdear_site.test-%d", ri), "url", fmt.Sprintf("http://updated.test-%d.com", ri)),
 				),
 			},
@@ -32,7 +50,7 @@ func TestAccOhdearSiteLifecycle(t *testing.T) {
 func testConfigForOhdearSiteCreate(rInt int) string {
 	return fmt.Sprintf(`
 resource "ohdear_site" "test-%d" {
-  team_id  = "1775"
+  team_id  = 1820
   url      = "http://www.test-%d.com"
 }
 `, rInt, rInt)
@@ -41,7 +59,7 @@ resource "ohdear_site" "test-%d" {
 func testConfigForOhdearSiteUpdate(rInt int) string {
 	return fmt.Sprintf(`
 resource "ohdear_site" "test-%d" {
-  team_id  = "1775"
+  team_id  = 1820
   url      = "http://updated.test-%d.com"
 }`, rInt, rInt)
 }
