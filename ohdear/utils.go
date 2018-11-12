666856fd040ff916c5ace026296e9b89e9aab7ff
Andy <andygertjejansen@gmail.com>
2018-11-12 13:47:45 -0600

    Wrap up provider (#3)
    * Wrapping up provider
    
    * Use site ID as site rather then URL
    * Add update lifecycle for disabling/enabling checks
    * Ignore vendor directory
    * Add check destroy function to avoid dangling resources
    * Check for actual resource downstream
    * Use api_url config
    * Add the ability to default checks (may need to consider adding support
    for http default since cert checks will be removed by the API)
    * Add checks update test
    
    * Add travis build
    
    * SDK Merged, updating constraint for dependency

---
+++ ohdear/utils.go
@@ -0,0 +1,36 @@
+package ohdear
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+
+	return false
+}
+
+func convertInterfaceToStringArr(purportedList interface{}) []string {
+	var arr []string
+	rawArr, ok := purportedList.([]interface{})
+
+	if ok {
+		arr = make([]string, len(rawArr))
+		for i, thing := range rawArr {
+			arr[i] = thing.(string)
+		}
+	}
+
+	return arr
+}
+
+// Converts interface to string array, if there are no elements it returns nil to conform with optional properties.
+func convertInterfaceToStringArrNullable(purportedList interface{}) []string {
+	arr := convertInterfaceToStringArr(purportedList)
+
+	if len(arr) < 1 {
+		return nil
+	}
+
+	return arr
+}
