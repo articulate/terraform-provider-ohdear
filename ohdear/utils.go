4b0929877ebec4b3a778558dec0fb31f86924a4a
Matt Loberg <mloberg@articulate.com>
2021-07-23 13:34:35 -0500

    refactor: remove deprecations for Terraform SDK v2
    Update all deprecated usages to the new methods/signatures. Clean up tests
    
    fix #19

--- ohdear/utils.go
+++ ohdear/utils.go
@@ -1,5 +1,11 @@
 package ohdear
 
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+)
+
 func contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
@@ -8,4 +14,14 @@ func contains(s []string, e string) bool {
 	}
 
 	return false
+}
+
+func diagErrorf(err error, format string, a ...interface{}) diag.Diagnostics {
+	return diag.Diagnostics{
+		diag.Diagnostic{
+			Severity: diag.Error,
+			Summary:  fmt.Sprintf(format, a...),
+			Detail:   err.Error(),
+		},
+	}
 }
