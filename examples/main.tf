9c95012a83653178bf34136df2c7b14e9581a089
Matt Loberg <mloberg@articulate.com>
2021-07-29 13:08:54 -0500

    docs: improve documentation generation
    Cleanup schema descriptions and examples. Configure documentation to be auto-generated with `make
    generate`

--- examples/main.tf
+++
@@ -1,4 +0,0 @@
-resource "ohdear_site" "test" {
-  team_id = 1234 # replace with your team id
-  url     = "https://example.com"
-}
---
+++ examples/resources/ohdear_site/import.sh
@@ -0,0 +1,2 @@
+# import using the Site ID found on the settings page
+terraform import ohdear_site.test 1234
